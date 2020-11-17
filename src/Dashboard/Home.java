764d3f0f3cb79305b73a39276950f4752ef3700e
nahuelchavez <46384889+nahuelchavez@users.noreply.github.com>
2020-11-17 20:59:40 -0300

    Add files via upload

---
+++ src/Dashboard/Home.java
@@ -0,0 +1,11 @@
+package Dashboard;
+
+import javax.swing.*;
+
+public class Home {
+    private JTextField homeTextField;
+    private JButton sociosButton;
+    private JButton operacionesButton;
+    private JButton documentacionButton;
+    private JButton otrosButton;
+}
---
+++ src/Dashboard/Socios.java
@@ -0,0 +1,51 @@
+package Dashboard;
+
+import javax.swing.*;
+
+public class Socios {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextField CUITTextField1;
+    private JTextArea textArea1;
+    private JTextField razonSocialTextField;
+    private JTextArea textArea2;
+    private JTextField fechaInicioTextField;
+    private JTextArea textArea3;
+    private JTextField tipoTextField;
+    private JTextArea textArea4;
+    private JTextField actividadPrincipalTextField;
+    private JTextArea textArea5;
+    private JTextField direccionTextField;
+    private JTextArea textArea6;
+    private JTextField telefonoTextField;
+    private JTextArea textArea7;
+    private JTextField correoTextField;
+    private JTextArea textArea8;
+    private JTextField estadoTextField;
+    private JCheckBox estaAprobadoCheckBox;
+    private JComboBox comboBox1;
+    private JTextField CUITTextField;
+    private JTextField razonSocialTextField1;
+    private JTextArea textArea9;
+    private JTextField fechaInicioTextField1;
+    private JTextField tipoTextField1;
+    private JTextArea textArea10;
+    private JTextField actividadPrincipalTextField1;
+    private JTextArea textArea11;
+    private JTextArea textArea12;
+    private JTextField direccionTextField1;
+    private JTextArea textArea13;
+    private JTextField telefonoTextField1;
+    private JTextArea textArea14;
+    private JTextField correoTextField1;
+    private JTextField estadoTextField1;
+    private JTextArea textArea15;
+    private JCheckBox estaAprobadoCheckBox1;
+    private JButton editarButton;
+    private JTextField ingreseElCUITTextField;
+    private JTextArea textArea16;
+    private JCheckBox estaSeguroCheckBox;
+    private JButton borrarButton;
+    private JList list1;
+    private JButton agregarButton;
+}
